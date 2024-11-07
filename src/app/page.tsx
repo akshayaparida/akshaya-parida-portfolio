5507184e97e475804251d93b048bd7e08e888f4e
Akshaya Parida <akshayaparida2811@gmail.com>
2024-11-07 21:53:03 +0530

    added 3d model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 // app/page.tsx
 import Image from "next/image";
+
 import bg from "../../public/background/home-background.png";
 import RenderModel from "../components/RenderModel";
 import Computer from "../components/models/Computer";
@@ -9,12 +10,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between relative">
       <Image
-        priority
+      
         sizes="100vw"
         src={bg}
         alt="background-image"
         fill
-        className="-z-50 w-full h-full object-cover object-center opacity-50"
+        className="-z-50 w-full h-full object-cover object-center opacity-20"
       />
       
       <div className="w-full h-screen">
--- src/components/models/Computer.tsx
+++ src/components/models/Computer.tsx
@@ -1,22 +1,38 @@
 "use client"
 
-import * as React from 'react'
+import React, { useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
 import { GroupProps } from '@react-three/fiber'
 import * as THREE from 'three'
+import { useFrame } from '@react-three/fiber'
 
 interface ComputerProps extends GroupProps {
   // Any additional props you want to pass can be added here
 }
 
 export default function Computer(props: ComputerProps) {
+  const modelRef = useRef<THREE.Group>(null) 
+
   const { nodes, materials } = useGLTF('/models/computer.glb')
+
+  useFrame((state) => {
+    if (modelRef.current) {
+      modelRef.current.position.x = 0.50 + Math.sin(state.clock.elapsedTime) * 0.50
+    }
+  })
 
   // Typecast the node to THREE.Mesh to access the geometry
   const mesh = nodes.retro_computer_setup_retro_computer_setup_Mat_0 as THREE.Mesh
 
   return (
-    <group {...props} dispose={null}>
+    <group
+      {...props}
+      ref={modelRef}
+      position={[0, -0.5, 0]}
+      scale={[0.05, 0.05, 0.05]}
+      rotation={[0.20, 0, 0]}
+      dispose={null}
+    >
       <mesh
         name="retro_computer_setup_retro_computer_setup_Mat_0"
         castShadow
